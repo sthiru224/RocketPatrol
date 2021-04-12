e26fcb688cd87743578d66c79dff4fe4381587c0
sthiru224 <sthiru@ucsc.edu>
2021-04-12 10:03:29 -0700

    added Rocket stuff

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -5,5 +5,7 @@ let config = {
     scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
